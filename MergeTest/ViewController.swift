7ffb4f462556bb583ebd516dae456e850cfe821f
Kai Oelfke <k.oelfke@me.com>
2019-11-04 09:51:10 +0100

    Initial Commit

---
+++ MergeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MergeTest
+//
+//  Created by Kai Oelfke on 04.11.19.
+//  Copyright © 2019 Kai Oelfke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
